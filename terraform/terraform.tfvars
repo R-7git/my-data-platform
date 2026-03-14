0b8623446e18e50849188d1bad91144822eb929e
R-7git <roshansingha4@gmail.com>
2026-03-14 16:44:53 +0530

    fix: add tfvars to automate terraform plan

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+snowflake_account  = "BKVGNQZ-UO15536"
+snowflake_user     = "ROSHAN"
+snowflake_password = "Rosh20090798395@"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,12 @@
 variable "snowflake_account" {
-  description = "BKVGNQZ-UO15536"
-  type        = string
+  type = string
 }
 
 variable "snowflake_user" {
-  description = "ROSHAN"
-  type        = string
+  type = string
 }
 
 variable "snowflake_password" {
-  description = "Rosh20090798395@"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
