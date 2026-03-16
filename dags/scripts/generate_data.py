f4c0b3a5c8b76227e8f36b8d376362e69e03dac8
R-7git <roshansingha4@gmail.com>
2026-03-16 13:01:45 +0530

    kjsrwvzxa

--- dags/scripts/generate_data.py
+++ dags/scripts/generate_data.py
@@ -14,7 +14,7 @@ ACCESS_KEY = 'minioadmin'
 SECRET_KEY = 'minioadmin' 
 BUCKET_NAME = 'raw-data-lake'
 
-def generate_and_upload_customers(num_records=1008): 
+def generate_and_upload_customers(num_records=108): 
     """
     Simulates an extraction from a CRM system.
     """
